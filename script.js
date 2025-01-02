d9636655d4bdfad7d0c6505a02e79eb084b1dc51
Lunix73 <linus.v.leitz@gmail.com>
2025-01-02 15:07:59 +0100

    portfolio update

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 let knapp = document.createElement('button');
 knapp.innerText = "Tryck för att se en bild!";
+knapp.addEventListener("click", function() { visaBild()});
+document.getElementById('bild').appendChild(knapp);
+
+function visaBild() {
+    let foto = document.createElement('img');
+    foto.src='images/fiol.jpg';
+    document.getElementById('bild').replaceChildren(foto);
+}
